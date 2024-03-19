f380653dac54cf60d711a7dc863b6115db0bc582
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-03-19 18:26:22 +0100

    Added version declaration

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 
 #include <pybind11/pybind11.h>
 
+#include <xmipp4/core_version.hpp>
+
+#include <sstream>
+
 namespace py = pybind11;
+using namespace xmipp4;
+
+static std::string version_to_string(version ver)
+{
+    std::ostringstream oss;
+    oss << ver;
+    return oss.str();
+}
 
 PYBIND11_MODULE(xmipp4, m) {
-
+    m.attr("__version__") = version_to_string(get_core_version());
 }
