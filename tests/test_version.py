dc0c0559ee36e30df8490ec8e8d02b0829f5f8f7
ssalinasmartin@gmail.com <ssalinasmartin@gmail.com>
2025-04-06 20:39:01 +0200

    Completed version tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -19,6 +19,7 @@
 #  All comments concerning this program package may be sent to the
 #  e-mail address 'xmipp@cnb.csic.es'
 # ***************************************************************************
+import pickle
 
 import xmipp4
 
@@ -29,3 +30,43 @@ def test_version_constructor():
       v.major, v.minor, v.patch
     ) == (1234, 567, 890)
   )
+
+def test_versions_are_equal():
+  v1 = xmipp4.Version(1234, 567, 890)
+  v2 = xmipp4.Version(1234, 567, 890)
+  assert v1 == v2
+
+def test_versions_are_not_equal():
+  v1 = xmipp4.Version(1234, 567, 890)
+  v2 = xmipp4.Version(1234, 567, 891)
+  assert v1 != v2
+
+def test_version_is_less():
+  v1 = xmipp4.Version(1234, 567, 890)
+  v2 = xmipp4.Version(1234, 567, 891)
+  assert v1 < v2
+
+def test_version_is_less_or_equal():
+  v1 = xmipp4.Version(1234, 567, 890)
+  v2 = xmipp4.Version(1234, 567, 890)
+  assert v1 <= v2
+
+def test_version_is_greater():
+  v1 = xmipp4.Version(1234, 567, 890)
+  v2 = xmipp4.Version(1234, 567, 891)
+  assert v2 > v1
+
+def test_version_is_greater_or_equal():
+  v1 = xmipp4.Version(1234, 567, 890)
+  v2 = xmipp4.Version(1234, 567, 890)
+  assert v1 >= v2
+
+def test_version_to_string():
+  v = xmipp4.Version(1234, 567, 890)
+  assert str(v) == "1234.567.890"
+
+def test_version_pickle():
+  v = xmipp4.Version(1234, 567, 890)
+  pickled = pickle.dumps(v)
+  unpickled = pickle.loads(pickled)
+  assert v == unpickled
