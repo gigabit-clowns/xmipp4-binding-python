031974c1b9b7b9b1ab13684c346c27b7b6454b85
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-04-08 12:45:55 +0200

    Simplified code

--- python/xmipp4/load_core.py
+++ python/xmipp4/load_core.py
@@ -2,9 +2,7 @@ from typing import List, Set
 from enum import Enum
 import os
 import sys
-import sysconfig
 import ctypes
-import itertools
 
 class Platform(Enum):
     LINUX = "linux"
@@ -24,8 +22,7 @@ def __get_platform() -> Platform:
     else:
         raise RuntimeError(f"Unsupported platform: {sys.platform}")
 
-def __get_library_filename(name: str) -> str:
-    platform = __get_platform()
+def __get_library_filename(name: str, platform: Platform) -> str:
     if platform == Platform.LINUX:
         return f"lib{name}.so"
     elif platform == Platform.MACOS:
@@ -33,11 +30,10 @@ def __get_library_filename(name: str) -> str:
     elif platform == Platform.WINDOWS:
         return f"{name}.dll"
 
-def __get_library_directory_names() -> List[str]:
+def __get_library_directory_names(platform: Platform) -> List[str]:
     """
     Get the library directory names based on the platform.
     """
-    platform = __get_platform()
     if platform == Platform.LINUX:
         return ["lib", "lib64"]
     elif platform == Platform.MACOS:
@@ -45,25 +41,20 @@ def __get_library_directory_names() -> List[str]:
     elif platform == Platform.WINDOWS:
         return ["bin"]
 
-def __get_directory_prefixes() -> List[str]:
-    """
-    Get possible prefixes for the library search paths.
-    """
-    return [sysconfig.get_path("data")]
-
 def __load_library(name: str) -> ctypes.CDLL:
     """
     Heuristically find and load a library with the specified name.
     """
-    filename = __get_library_filename(name)
+    platform = __get_platform()
+    filename = __get_library_filename(name, platform)
     try:
         return ctypes.CDLL(filename)
     except OSError:
         pass
 
-    prefixes = __get_directory_prefixes()
-    lib_directories = __get_library_directory_names()
-    for prefix, lib_directory in itertools.product(prefixes, lib_directories):
+    prefix = sys.prefix
+    lib_directories = __get_library_directory_names(platform)
+    for lib_directory in lib_directories:
         path = os.path.join(prefix, lib_directory, filename)
         if os.path.exists(path):
             try:
