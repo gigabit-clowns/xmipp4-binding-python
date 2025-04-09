4d4409b5233ace433543d4756b30c43aa03d5037
Martin Salinas <ssalinasmartin@gmail.com>
2025-04-09 09:45:25 +0200

    Communicator manager tests

---
+++ tests/communication/test_communicator_manager.py
@@ -0,0 +1,45 @@
+#***************************************************************************
+# Authors:     Oier Lauzirika Zarrabeitia (oierlauzi@bizkaia.eu)
+#              Martín Salinas Antón (ssalinasmartin@gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# 02111-1307  USA
+#
+#  All comments concerning this program package may be sent to the
+#  e-mail address 'xmipp@cnb.csic.es'
+# ***************************************************************************
+import pytest
+
+import xmipp4
+
+def test_returns_communicator_manager(__setup_interface_registry):
+  assert xmipp4.communication.get_communicator_manager(
+    __setup_interface_registry
+  ) is not None
+
+def test_always_returns_same_communicator_manager(__setup_interface_registry):
+  cm1 = xmipp4.communication.get_communicator_manager(__setup_interface_registry)
+  cm2 = xmipp4.communication.get_communicator_manager(__setup_interface_registry)
+  assert cm1 is cm2
+
+def test_returns_default_backends(__setup_interface_registry):
+  dm = xmipp4.compute.get_device_manager(
+    __setup_interface_registry
+  )
+  assert dm.backends == ['dummy']
+
+@pytest.fixture
+def __setup_interface_registry():
+  return xmipp4.InterfaceRegistry()
