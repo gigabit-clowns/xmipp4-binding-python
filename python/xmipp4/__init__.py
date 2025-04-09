ed8d9b70157e70c7781c7f8fe5a95e17f87c7edd
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-04-09 10:30:50 +0200

    Renaming some variables (#19)

--- python/xmipp4/__init__.py
+++ python/xmipp4/__init__.py
@@ -24,7 +24,7 @@ from .load_core import load_core
 # Heuristically load the core library before importing the binding.
 __core_lib = load_core()
 
-from ._core import (
+from ._core_binding import (
     __doc__, __version__, 
     communication, compute, image,
     InterfaceRegistry,
