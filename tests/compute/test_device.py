8b51ec536d5bbc9b1b39a4eaa21c2491754efce8
Martin Salinas <ssalinasmartin@gmail.com>
2025-07-17 16:26:05 +0200

    Substitute verbose headers with SPDX identifier for lincense (#53)
    Also:
    - Removed redundant doxygen file headers
    - Removed double line breaks

--- tests/compute/test_device.py
+++ tests/compute/test_device.py
@@ -1,32 +1,12 @@
-#***************************************************************************
-# Authors:     Oier Lauzirika Zarrabeitia (oierlauzi@bizkaia.eu)
-#              Martín Salinas Antón (ssalinasmartin@gmail.com)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-# 02111-1307  USA
-#
-#  All comments concerning this program package may be sent to the
-#  e-mail address 'xmipp@cnb.csic.es'
-# ***************************************************************************
+# SPDX-License-Identifier: GPL-3.0-only
+
 import pytest
 
 import xmipp4
 
 def test_returns_device(__setup_device_manager):
   assert __setup_device_manager.create_device(
-    xmipp4.compute.DeviceIndex('host', 0)
+    xmipp4.compute.DeviceIndex('cpu', 0)
   ) is not None
 
 @pytest.mark.parametrize(
@@ -38,9 +18,9 @@ def test_returns_device(__setup_device_manager):
     pytest.param(3, id='Three queues requested')
   ]
 )
-def test_returns_expected_queues_for_host(__setup_device_manager, n_queues):
+def test_returns_expected_queues_for_cpu(__setup_device_manager, n_queues):
   device = __setup_device_manager.create_device(
-    xmipp4.compute.DeviceIndex('host', 0),
+    xmipp4.compute.DeviceIndex('cpu', 0),
     desired_queue_count=n_queues
   )
   queue_pool = device.get_queue_pool()
--- tests/compute/test_device_manager.py
+++ tests/compute/test_device_manager.py
@@ -1,25 +1,5 @@
-#***************************************************************************
-# Authors:     Oier Lauzirika Zarrabeitia (oierlauzi@bizkaia.eu)
-#              Martín Salinas Antón (ssalinasmartin@gmail.com)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-# 02111-1307  USA
-#
-#  All comments concerning this program package may be sent to the
-#  e-mail address 'xmipp@cnb.csic.es'
-# ***************************************************************************
+# SPDX-License-Identifier: GPL-3.0-only
+
 import pytest
 
 import xmipp4
@@ -38,13 +18,13 @@ def test_returns_default_backends(__setup_interface_catalog):
   dm = xmipp4.compute.get_device_manager(
     __setup_interface_catalog
   )
-  assert dm.backends == ['host']
+  assert dm.backends == ['cpu']
 
 def test_returns_default_devices(__setup_interface_catalog):
   dm = xmipp4.compute.get_device_manager(
     __setup_interface_catalog
   )
-  assert dm.devices == [xmipp4.compute.DeviceIndex('host', 0)]
+  assert dm.devices == [xmipp4.compute.DeviceIndex('cpu', 0)]
 
 @pytest.fixture
 def __setup_interface_catalog():
