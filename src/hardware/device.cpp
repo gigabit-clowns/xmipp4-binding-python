8422a9a4c7fd791169cd4c43c4676120c4248d0b
ssalinasmartin@gmail.com <ssalinasmartin@gmail.com>
2025-11-02 12:02:03 +0100

    Removed device memory allocator

--- src/hardware/device.cpp
+++ src/hardware/device.cpp
@@ -3,7 +3,6 @@
 #include "device.hpp"
 
 #include <xmipp4/core/hardware/device.hpp>
-#include <xmipp4/core/hardware/device_memory_allocator.hpp>
 #include <xmipp4/core/hardware/host_memory_allocator.hpp>
 #include <xmipp4/core/hardware/host_to_device_transfer.hpp>
 #include <xmipp4/core/hardware/device_to_host_transfer.hpp>
@@ -21,11 +20,6 @@ namespace py = pybind11;
 void bind_device(pybind11::module_ &m)
 {
     py::class_<device, std::shared_ptr<device>>(m, "Device")
-        .def(
-            "create_device_memory_allocator", 
-            &device::create_device_memory_allocator,
-            py::keep_alive<0, 1>()
-        )
         .def(
             "create_host_memory_allocator", 
             &device::create_host_memory_allocator,
