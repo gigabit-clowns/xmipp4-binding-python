e9ed589e66ced58bd70d8c068b0753f2f25fb52e
ssalinasmartin@gmail.com <ssalinasmartin@gmail.com>
2025-11-21 23:25:13 +0100

    Adapted communicator manager

--- src/communication/communicator_manager.cpp
+++ src/communication/communicator_manager.cpp
@@ -26,24 +26,9 @@ namespace py = pybind11;
 void bind_communicator_manager(pybind11::module_ &m)
 {
 	py::class_<communicator_manager>(m, "CommunicatorManager")
-		.def_property_readonly(
-			"backends",
-			[] (const communicator_manager &manager) -> std::vector<std::string>
-			{
-				std::vector<std::string> backends;
-				manager.enumerate_backends(backends);
-				return backends;
-			}
-		)
 		.def(
 			"create_world_communicator", 
-			&communicator_manager::create_world_communicator, 
-			py::arg("name"),
-			py::keep_alive<0, 1>()
-		)
-		.def(
-			"create_preferred_world_communicator", 
-			&communicator_manager::create_preferred_world_communicator,
+			&communicator_manager::create_world_communicator,
 			py::keep_alive<0, 1>()
 		);
 
