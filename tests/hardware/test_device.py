489a0673559af63dbe5c7d6da62de7c982d8284b
ssalinasmartin@gmail.com <ssalinasmartin@gmail.com>
2025-11-22 18:32:47 +0100

    WIP

--- tests/hardware/test_device.py
+++ tests/hardware/test_device.py
@@ -1,15 +1,24 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
+import unittest
+from unittest.mock import patch
+
 import pytest
 
 import xmipp4
 
-def test_returns_device(__setup_device_manager):
-  assert __setup_device_manager.create_device(
-    xmipp4.hardware.DeviceIndex('cpu', 0)
-  ) is not None
+def test_should_allow_inheriting_from_device(__setup_device):
+	assert isinstance(__setup_device, xmipp4.hardware.Device)
+
+def test_whatever(__setup_device):
+	assert __setup_device.create_device_queue() == 0
 
 @pytest.fixture
-def __setup_device_manager():
-  ir = xmipp4.ServiceCatalog()
-  return xmipp4.hardware.get_device_manager(ir)
+def __setup_device():
+	class DeviceMock(xmipp4.hardware.Device):
+		pass
+	
+	mymock = DeviceMock()
+	mymock.create_device_queue = unittest.mock.MagicMock(return_value=42)
+
+	return xmipp4.hardware.launder(mymock)
